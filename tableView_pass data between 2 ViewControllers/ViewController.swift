8f2db2adb8d6972d4bb6057a5205e94b78ac18f9
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-07-06 12:03:54 -0600

    add comment

--- tableView_pass data between 2 ViewControllers/ViewController.swift
+++ tableView_pass data between 2 ViewControllers/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
+
+// Drag 2 View Controller
+
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didGetNotification(_:)), name: NSNotification.Name("text"), object: nil)
     }
 
-
+    // Create another function :
+    
+    @objc func didGetNotification(_ notification: Notification){
+        
+        let text = notification.object as! String?
+        
+        label.text = text
+    }
+    
+    
+    @IBAction func didTapButton(){
+        
+        let vc = storyboard?.instantiateViewController(identifier:"other") as! OtherViewController
+        
+        vc.modalPresentationStyle = .fullScreen
+        
+ //       vc.completionHandler = {text in
+        
+   //         self.label.text = text
+ //   }
+        present(vc, animated: true)
+    
 }
 
+}
